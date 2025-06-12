3bd0dd403d2cb33cd5a0f29e51dcf2e1bf9980bb
Sealybla <lijialin2018@gmail.com>
2025-06-12 15:32:36 -0500

    fixed again

--- models.py
+++ models.py
@@ -125,22 +125,41 @@ class MPMC_net(nn.Module):
         return out
 
     def L2center(self, x):
-        N = x.size(1)
+        n = x.size(1)
         dim = x.size(2)
 
-        prod1 = 0.5*(torch.abs(x - 0.5) - (torch.abs(x - 0.5))**2.)
-        prod1 = torch.prod(prod1, dim=2)
-        sum1 = torch.sum(prod1, dim=1)
+        # Term 1: (13/12)^d
+        term1 = (13.0 / 12.0) ** dim
 
-        prod2 = 0.5*(torch.abs(x[:, :, None, :] - 0.5) 
-                     + torch.abs(x[:, None, :, :] - 0.5) 
-                     - torch.abs(x[:, :, None, :] - x[:, None, :, :]))
-        product = torch.prod(prod2, dim=3)
-        sum2 = torch.sum(product, dim=(1, 2))
-        two_div_N = 2. / N
-        
-        out = torch.sqrt(math.pow(12., -dim) - two_div_N * sum1 + 1. / math.pow(N, 2.) * sum2)
+        # Term 2: -2/n * sum_i ∏_j (1 + 0.5|x_ij - 0.5| - 0.5|x_ij - 0.5|^2)
+        sum1 = torch.abs(x - 0.5)
+        prod1 = 1.0 + 0.5 * sum1 - 0.5 * (sum1 ** 2)
+        prod2 = torch.prod(prod1, dim=2)
+        sum2 = torch.sum(prod2, dim=1)
+        term2 = -(2.0 / n) * sum2
+
+        # Term 3: 1/(n^2) * sum_{i,j} ∏_k (1 + 0.5|x_ik - 0.5| + 0.5|x_jk - 0.5| - 0.5|x_ik - x_jk|)
+        x_i = x.unsqueeze(2)
+        x_j = x.unsqueeze(1)
+        prod3 = (
+            1.0
+            + 0.5 * torch.abs(x_i - 0.5)
+            + 0.5 * torch.abs(x_j - 0.5)
+            - 0.5 * torch.abs(x_i - x_j)
+        )
+        prod4 = torch.prod(prod3, dim=3)
+        sum3 = torch.sum(prod4, dim=(1, 2))
+        term3 = (1.0 / (n * n)) * sum3
+
+        # Combine terms for squared discrepancy
+        squared_discrepancy = term1 + term2 + term3
+
+        # Return L2 discrepancy (square root of squared discrepancy)
+        out = torch.sqrt(squared_discrepancy)
         return out
+
+        
+        
     
     def L2ext(self, x):
         N = x.size(1)
--- utils.py
+++ utils.py
@@ -29,21 +29,37 @@ def hickernell_all_emphasized(x,dim_emphasize):
 
 
 def L2center(x):
-        N = x.size(1)
+        n = x.size(1)
         dim = x.size(2)
 
-        prod1 = 0.5*(torch.abs(x - 0.5) - (torch.abs(x - 0.5))**2.)
-        prod1 = torch.prod(prod1, dim=2)
-        sum1 = torch.sum(prod1, dim=1)
+        # Term 1: (13/12)^d
+        term1 = (13.0 / 12.0) ** dim
 
-        prod2 = 0.5*(torch.abs(x[:, :, None, :] - 0.5) 
-                     + torch.abs(x[:, None, :, :] - 0.5) 
-                     - torch.abs(x[:, :, None, :] - x[:, None, :, :]))
-        product = torch.prod(prod2, dim=3)
-        sum2 = torch.sum(product, dim=(1, 2))
-        two_div_N = 2. / N
-        
-        out = math.pow(12., -dim) - two_div_N * sum1 + 1. / math.pow(N, 2.) * sum2
+        # Term 2: -2/n * sum_i ∏_j (1 + 0.5|x_ij - 0.5| - 0.5|x_ij - 0.5|^2)
+        sum1 = torch.abs(x - 0.5)
+        prod1 = 1.0 + 0.5 * sum1 - 0.5 * (sum1 ** 2)
+        prod2 = torch.prod(prod1, dim=2)
+        sum2 = torch.sum(prod2, dim=1)
+        term2 = -(2.0 / n) * sum2
+
+        # Term 3: 1/(n^2) * sum_{i,j} ∏_k (1 + 0.5|x_ik - 0.5| + 0.5|x_jk - 0.5| - 0.5|x_ik - x_jk|)
+        x_i = x.unsqueeze(2)
+        x_j = x.unsqueeze(1)
+        prod3 = (
+            1.0
+            + 0.5 * torch.abs(x_i - 0.5)
+            + 0.5 * torch.abs(x_j - 0.5)
+            - 0.5 * torch.abs(x_i - x_j)
+        )
+        prod4 = torch.prod(prod3, dim=3)
+        sum3 = torch.sum(prod4, dim=(1, 2))
+        term3 = (1.0 / (n * n)) * sum3
+
+        # Combine terms for squared discrepancy
+        squared_discrepancy = term1 + term2 + term3
+
+        # Return L2 discrepancy (square root of squared discrepancy)
+        out = torch.sqrt(squared_discrepancy)
         return out
     
 def L2ext(x):
@@ -58,7 +74,7 @@ def L2ext(x):
     product = torch.prod(prod2, dim = 3)
     sum2 = torch.sum(product, dim = (1,2))
 
-    out = math.pow(12., -dim) - (2. / N) * sum1 + math.pow(N, - 2.) * sum2
+    out = torch.sqrt(math.pow(12., -dim) - (2. / N) * sum1 + math.pow(N, - 2.) * sum2)
     return out
     
 def L2per(x):
@@ -69,7 +85,7 @@ def L2per(x):
     product = torch.prod(prod2, dim = 3)
     sum2 = torch.sum(product, dim = (1,2))
 
-    out = - math.pow(3., -dim) + math.pow(N, - 2.) * sum2
+    out = torch.sqrt(- math.pow(3., -dim) + math.pow(N, - 2.) * sum2)
     return out
     
 def L2sym(x): 
@@ -84,5 +100,5 @@ def L2sym(x):
     product = torch.prod(prod2, dim = 3)
     sum2 = torch.sum(product, dim = (1,2))
 
-    out = math.pow(12., -dim) - (2. / N) * sum1 + math.pow(N, - 2.) * sum2
+    out = torch.sqrt(math.pow(12., -dim) - (2. / N) * sum1 + math.pow(N, - 2.) * sum2)
     return out
