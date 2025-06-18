f726631b4c55e166228c7650f43a5c44a46a3aad
Sealybla <lijialin2018@gmail.com>
2025-06-18 10:41:54 -0500

    256 sym extr dispersion

--- run_npy.py
+++ run_npy.py
@@ -22,38 +22,38 @@ data = np.load(r'D:\work\Research\IITSURE\coding\MPMC\outputs\dim_2\nsamples_16\
 
 #calculate dispersion:
  
-# losses = ['LfL2ext']
-# configs = [[16, 32], [32, 32], [64,32], [128,64]]
-# for loss in losses:
-#     for nsamples, nhid in configs:
-#         path = fr'D:\work\Research\IITSURE\coding\MPMC_Copy\outputs\dim_2\nsamples_{nsamples}\nhid_{nhid}\{loss}.npy'
-#         data = np.load(path)
-#         Path('dispersion/data').mkdir(parents=True, exist_ok=True)
-#         Path('dispersion/pics').mkdir(parents=True, exist_ok=True)
-#         pic_path = f'dispersion/pics/{loss}/plot_nsamples{nsamples}.png'
-#         max_vol, best_box = dispersion(data.squeeze(), save_path = pic_path)
+losses = ['LfL2ext', 'LfL2sym']
+configs = [[256,32]]
+for loss in losses:
+    for nsamples, nhid in configs:
+        path = fr'D:\work\Research\IITSURE\coding\MPMC_Copy\outputs\dim_2\nsamples_{nsamples}\nhid_{nhid}\{loss}.npy'
+        data = np.load(path)
+        Path('dispersion/data').mkdir(parents=True, exist_ok=True)
+        Path('dispersion/pics').mkdir(parents=True, exist_ok=True)
+        pic_path = f'dispersion/pics/{loss}/plot_nsamples{nsamples}.png'
+        max_vol, best_box = dispersion(data.squeeze(), save_path = pic_path)
         
-#         f = open('dispersion/data/' + loss + '.txt', 'a')
-#         f.write(f"nsamples, nhid: ({nsamples},{nhid})\n")
-#         f.write(f"Dispersion: {max_vol}\n")
-#         f.write(f"Box: {best_box}\n\n")
+        f = open('dispersion/data/' + loss + '.txt', 'a')
+        f.write(f"nsamples, nhid: ({nsamples},{nhid})\n")
+        f.write(f"Dispersion: {max_vol}\n")
+        f.write(f"Box: {best_box}\n\n")
 
-#         f.close()
+        f.close()
 
 
 #calculate l_extreme for random points (16):
 # print(Linfextr(torch.from_numpy(data)))
 
 #Sobol
-points = [32, 64, 128]
-sobol_gen = Sobol(dimension = 2, randomize = None)
-for n in points:
-    data = sobol_gen.gen_samples(n)
-    pic_path = f'dispersion/Sobol/pics/plot_nsamples{n}.png'
-    max_vol, best_box = dispersion(data.squeeze(), save_path = pic_path)
-    f = open('dispersion/Sobol/data.txt', 'a')
-    f.write(f"nsamples: ({n})\n")
-    f.write(f"Dispersion: {max_vol}\n")
-    f.write(f"Box: {best_box}\n\n")
-    f.close()
+# points = [32, 64, 128]
+# sobol_gen = Sobol(dimension = 2, randomize = None)
+# for n in points:
+#     data = sobol_gen.gen_samples(n)
+#     pic_path = f'dispersion/Sobol/pics/plot_nsamples{n}.png'
+#     max_vol, best_box = dispersion(data.squeeze(), save_path = pic_path)
+#     f = open('dispersion/Sobol/data.txt', 'a')
+#     f.write(f"nsamples: ({n})\n")
+#     f.write(f"Dispersion: {max_vol}\n")
+#     f.write(f"Box: {best_box}\n\n")
+#     f.close()
 
