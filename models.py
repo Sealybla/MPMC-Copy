b29995fc91c809e7420a5251c25b4765cdf69687
Ally Pascual Kwan <aakpascual@gmail.com>
2025-07-15 21:21:55 -0500

    added weighted for training

--- models.py
+++ models.py
@@ -1,4 +1,113 @@
+import math
+import numpy as np
+from itertools import product 
 import torch
+from torch import nn
+from torch_cluster import radius_graph
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+from torch_geometric.nn import MessagePassing, InstanceNorm
+from utils import L2dis, L2ctr, L2ext, L2per, L2sym, L2ags, L2mix, L2dis_weighted, L2ctr_weighted, L2sym_weighted, L2per_weighted, L2ext_weighted, L2ags_weighted, L2mix_weighted
+
+
+class MPNN_layer(MessagePassing):
+    def __init__(self, ninp, nhid):
+        super(MPNN_layer, self).__init__()
+        self.ninp = ninp
+        self.nhid = nhid
+
+        self.message_net_1 = nn.Sequential(nn.Linear(2 * ninp, nhid),
+                                           nn.ReLU()
+                                           )
+        self.message_net_2 = nn.Sequential(nn.Linear(nhid, nhid),
+                                           nn.ReLU()
+                                           )
+        self.update_net_1 = nn.Sequential(nn.Linear(ninp + nhid, nhid),
+                                          nn.ReLU()
+                                          )
+        self.update_net_2 = nn.Sequential(nn.Linear(nhid, nhid),
+                                          nn.ReLU()
+                                          )
+        self.norm = InstanceNorm(nhid)
+
+    def forward(self, x, edge_index, batch):
+        x = self.propagate(edge_index, x=x)
+        x = self.norm(x, batch)
+        return x
+
+    def message(self, x_i, x_j):
+        message = self.message_net_1(torch.cat((x_i, x_j), dim=-1))
+        message = self.message_net_2(message)
+        return message
+
+    def update(self, message, x):
+        update = self.update_net_1(torch.cat((x, message), dim=-1))
+        update = self.update_net_2(update)
+        return update
+
+
+class MPMC_net(nn.Module):
+    def __init__(self, dim, nhid, nlayers, nsamples, nbatch, radius, loss_fn, weights, n_projections):
+        super(MPMC_net, self).__init__()
+        self.enc = nn.Linear(dim,nhid)
+        self.convs = nn.ModuleList()
+        for i in range(nlayers):
+            self.convs.append(MPNN_layer(nhid,nhid))
+        self.dec = nn.Linear(nhid,dim)
+        self.nlayers = nlayers
+        self.mse = torch.nn.MSELoss()
+        self.nbatch = nbatch
+        self.nsamples = nsamples
+        self.dim = dim
+        self.n_projections = n_projections
+        self.enc = nn.Linear(dim,nhid)
+        self.convs = nn.ModuleList()
+        for i in range(nlayers):
+            self.convs.append(MPNN_layer(nhid,nhid))
+        self.dec = nn.Linear(nhid,dim)
+        self.nlayers = nlayers
+        self.mse = torch.nn.MSELoss()
+        self.nbatch = nbatch
+        self.nsamples = nsamples
+        self.dim = dim
+        self.n_projections = n_projections
+
+        ## random shift
+        self.x = torch.rand(nsamples * nbatch, dim).to(device)
+
+        # store weights
+        self.weights = weights
+
+        batch = torch.arange(nbatch).unsqueeze(-1).to(device)
+        batch = batch.repeat(1, nsamples).flatten()
+        self.batch = batch
+        self.edge_index = radius_graph(self.x, r=radius, loop=True, batch=batch).to(device)
+
+        all_losses = {'L2dis', 'L2ctr', 'L2ext', 'L2per', 'L2sym', 'L2ags', 'L2mix', 'L2dis_weighted',
+                      'L2ctr_weighted', 'L2ext_weighted', 'L2per_weighted', 'L2sym_weighted', 'L2ags_weighted', 'L2mix_weighted'}
+        if loss_fn in all_losses:
+            self.loss_fn = globals()[loss_fn]
+        else:
+            raise ValueError(f"Loss function DNE: {loss_fn}")
+
+    def forward(self):
+        X = self.x
+        edge_index = self.edge_index
+
+        X = self.enc(X)
+        for i in range(self.nlayers):
+            X = self.convs[i](X,edge_index,self.batch)
+        X = torch.sigmoid(self.dec(X))
+        X = X.view(self.nbatch, self.nsamples, self.dim)
+
+        if self.weights is not None:
+            loss = torch.mean(self.loss_fn(X, self.weights))
+        else:
+            loss = torch.mean(self.loss_fn(X))
+        return loss, X
+
+
+
+"""import torch
 import math
 from torch import nn
 from torch_cluster import radius_graph
@@ -76,6 +185,8 @@ class MPMC_net(nn.Module):
             self.loss_fn = self.L2per
         elif loss_fn == 'L2sym':
             self.loss_fn = self.L2sym
+        elif loss_fn == 'L2avgs':
+            self.loss_fn = self.L2avgs
         elif loss_fn == 'L2mix':
             self.loss_fn = self.L2mix
         elif loss_fn == 'approx_hickernell':
@@ -244,6 +355,37 @@ class MPMC_net(nn.Module):
      out = torch.sqrt(math.pow(7./12., dim) - (2. / N) * sum1 + math.pow(N, -2.) * sum2)
      return out
     
+
+
+
+    def L2ags_weighted (self, x, gamma):
+        N = x.size(1)
+        dim = x.size(2)
+
+        term1 = torch.prod(1.0 + gamma**2 / 3.0)
+
+        g_term2 = gamma.view(1, 1, dim)
+        sum1 = g_term2 * (2. * x - 2. * x**2)
+        sum2 = 1.0 + sum1
+        prod1 = sum2 / 4. 
+        prod2 = torch.prod(prod1, dim=2)
+        sum_prod2 = torch.sum(prod2, dim=1)
+        term2 = -(2. / N) * sum_prod2
+
+        x_i = x.unsqueeze(2)
+        x_j = x.unsqueeze(1)
+        g_term3 = gamma.view(1, 1, 1, dim)   # need to reshape?
+        sum3 = g_term3 * torch.abs(x_i - x_j)
+        prod3 = torch.prod((1. - sum3) / 2., dim=3)
+        term3_sum = torch.sum(prod3, dim=(1, 2))
+        term3 = (1. / (N * N)) * term3_sum
+
+        out_sq = term1 + term2 + term3
+        out = torch.sqrt(torch.relu(out_sq))
+        
+        return out
+
+    
     def forward(self):
         X = self.x
         edge_index = self.edge_index
@@ -256,5 +398,4 @@ class MPMC_net(nn.Module):
         loss = torch.mean(self.loss_fn(X))
         return loss, X
     
-
-    
+"""
--- run_train.py
+++ run_train.py
@@ -4,7 +4,113 @@ import torch.optim as optim
 import numpy as np
 from pathlib import Path
 import argparse
-from utils import L2discrepancy, hickernell_all_emphasized, L2center, L2ext, L2per, L2sym, L2mix
+from utils import L2dis, L2ctr, L2ext, L2per, L2sym, L2ags, L2mix, L2dis_weighted, L2ctr_weighted, L2sym_weighted, L2per_weighted, L2ext_weighted, L2ags_weighted, L2mix_weighted
+from types import SimpleNamespace
+from tqdm import tqdm
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def train(args):
+    model = MPMC_net(args.dim, args.nhid, args.nlayers, args.nsamples, args.nbatch,
+                     args.radius, args.loss_fn, args.weights, args.n_projections).to(device)
+    optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+    best_loss = 10000.
+    patience = 0
+
+    start_reduce = 100000
+    reduce_point = 10
+
+    Path('results/dim_' + str(args.dim) + '/nsamples_'+str(args.nsamples)+'/nhid_'+str(args.nhid)).mkdir(parents=True, exist_ok=True)
+    Path('outputs/dim_' + str(args.dim) + '/nsamples_'+str(args.nsamples)+'/nhid_'+str(args.nhid)).mkdir(parents=True, exist_ok=True)
+
+    for epoch in tqdm(range(args.epochs), desc=f"Training: N={args.nsamples}, nhid={args.nhid}, loss={args.loss_fn}"):
+        model.train()
+        optimizer.zero_grad()
+        loss, X = model()
+        loss.backward()
+        optimizer.step()
+
+        if epoch % 100 == 0:
+            y = X.clone().detach()
+            
+            loss_func_name = args.loss_fn.replace('_weighted', '')
+            loss_func = globals()[loss_func_name]
+
+      
+            batched_discrepancies = loss_func(y)
+
+            min_discrepancy = torch.min(batched_discrepancies).item()
+
+            if min_discrepancy < best_loss:
+                best_loss = min_discrepancy
+                best_loss = min_discrepancy
+                f = open('results/dim_'+str(args.dim)+'/nsamples_'+str(args.nsamples)+'/nhid_'+str(args.nhid) + '/Lf'+str(args.loss_fn) + '.txt', 'a')
+                f.write(str(best_loss) + '\n')
+                f.close()
+
+                PATH = 'outputs/dim_'+str(args.dim)+'/nsamples_'+str(args.nsamples)+ '/nhid_' +str(args.nhid)+ '/Lf'+str(args.loss_fn) + '.npy'
+                y = y.detach().cpu().numpy()
+                np.save(PATH,y)
+
+            if (min_discrepancy > best_loss and (epoch + 1) >= args.start_reduce):
+                patience += 1
+
+            if (epoch + 1) >= args.start_reduce and patience == reduce_point:
+                patience = 0
+                args.lr /= 10.
+                for param_group in optimizer.param_groups:
+                    param_group['lr'] = args.lr
+
+            if (args.lr < 1e-6):
+                f = open('results/dim_'+str(args.dim)+'/nsamples_'+str(args.nsamples)+'/nhid_'+str(args.nhid) + '/Lf'+str(args.loss_fn) + '.txt', 'a')
+                f.write('### epochs: '+str(epoch) + '\n')
+                f.close()
+                break
+
+
+if __name__ == '__main__':
+    #me: 'L2dis_weighted', 'L2ctr_weighted', 'L2ext_weighted'
+    #you/Lijia: 'L2per_weighted', 'L2sym_weighted', 'L2ags_weighted', 'L2mix_weighted'
+
+    loss_functions = ['L2dis_weighted', 'L2ctr_weighted', 'L2ext_weighted',
+                      'L2per_weighted', 'L2sym_weighted', 'L2ags_weighted', 'L2mix_weighted']
+
+    for N in tqdm([8, 16, 32, 64, 128, 256], desc='Sample Sizes'):
+        for nh in tqdm([32], desc="Hidden Units", leave=False):
+            for l in tqdm(loss_functions, desc="Loss Fn", leave=False):
+                dim = 52
+
+                # 52 dimensions for 52 weeks in a year
+                gamma = torch.zeros(52, device=device)
+                gamma[0:3] = 1.0
+
+                args = {
+                    'lr': 0.001,
+                    'nlayers': 3,
+                    'weight_decay': 1e-6,
+                    'nhid': nh,
+                    'nbatch': 1,
+                    'epochs': 200000,
+                    'start_reduce': 100000,
+                    'radius': 0.35,
+                    'nsamples': N,
+                    'dim': dim,
+                    'loss_fn': l,
+                    'weights': gamma if 'weighted' in l else None,
+                    'n_projections': 15
+                }
+
+                args = SimpleNamespace(**args)
+                train(args)
+                print(f"Finished training with N = {N}, nhid = {nh}, loss_fun = {l}")
+
+"""from models import *
+import torch
+import torch.optim as optim
+import numpy as np
+from pathlib import Path
+import argparse
+from utils import L2discrepancy, hickernell_all_emphasized, L2center, L2dis,  L2ext, L2per, L2sym, L2mix, L2avgs, L2ags_weighted, L2ctr_weighted, L2dis_weighted, L2ext_weighted, L2mix_weighted, L2per_weighted, L2sym_weighted
 from types import SimpleNamespace
 from tqdm import tqdm 
 
@@ -44,8 +150,11 @@ def train(args):
                 batched_discrepancies = L2per(y.detach())
             elif args.loss_fn == 'L2sym':
                 batched_discrepancies = L2sym(y.detach())
+            elif args.loss_fn == 'L2avgs':
+                batched_discrepancies = L2avgs(y.detach())
             elif args.loss_fn == 'L2mix':
                 batched_discrepancies = L2mix(y.detach())
+            elif
             elif args.loss_fn == 'approx_hickernell':
                 ## compute sum over all projections with emphasized dimensionality:
                 batched_discrepancies = hickernell_all_emphasized(y.detach(),args.dim_emphasize)
@@ -83,9 +192,9 @@ def train(args):
 
 if __name__ == '__main__':
 
-    for N in tqdm([10,20,30,40,50,60,70,80,90,100,110,120], desc = 'Sample Sizes'):
+    for N in tqdm([100], desc = 'Sample Sizes'):
         for nh in tqdm([32], desc = "Hidden Units", leave = False):
-            for l in tqdm(['L2cen'], desc = "Loss Fn", leave = False):
+            for l in tqdm(['L2per', 'L2dis', 'L2avgs', 'L2ext', 'L2mix', 'L2per', 'L2sym'], desc = "Loss Fn", leave = False):
                 args = {
                     'lr': 0.001,                  # learning rate
                     'nlayers': 3,                 # number of GNN layers
@@ -143,3 +252,4 @@ if __name__ == '__main__':
     # args = parser.parse_args()
     # train(args)
 
+"""
